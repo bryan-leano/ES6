1bf8b1b32688fa78dcc9e452c6dbdea8a6155740
Bryan Leanoe <bryan.leano@gmail.com>
2018-12-04 15:13:40 -0700

    spread operator

--- script.js
+++ script.js
@@ -269,6 +269,7 @@ console.log(retirement);
 //////////////////////////////////////////
 // Lecture: Arrays
 
+/*
 const boxes = document.querySelectorAll('.box');
 
 // ES5
@@ -284,7 +285,7 @@ boxesArr5.forEach(function(cur) {
 const boxesArr6 = Array.from(boxes);
 Array.from(boxes).forEach(cur => cur.style.backgroundColor = 'dodgerblue');
 
-/*
+
 // ES5
 for (var i = 0 ;i < boxesArr5.length; i++) {
 
@@ -296,7 +297,7 @@ for (var i = 0 ;i < boxesArr5.length; i++) {
   boxesArr5[i].textContent = 'I changed to blue!';
 
 }
-*/
+
 
 // ES6
 for (const cur of boxesArr6) {
@@ -323,3 +324,36 @@ console.log(ages[full.indexOf(true)]);
 // ES6
 console.log(ages.findIndex(cur => cur >= 18));
 console.log(ages.find(cur => cur >= 18));
+*/
+
+
+//////////////////////////////////////
+// Lecture: Spread Operator
+
+function addFourAges (a, b, c, d) {
+  return a + b + c + d;
+};
+
+var sum1 = addFourAges(18, 30, 12, 21);
+console.log(sum1);
+
+// ES5
+ages = [18, 30, 12, 21];
+var sum2 = addFourAges.apply(null, ages);
+console.log(sum2);
+
+// ES6
+const sum3 = addFourAges(...ages);
+console.log(sum3);
+
+const familySmith = ['John', 'Jane', 'Mark'];
+const familyMiller = ['Mary', 'Bob', 'Ann'];
+
+const bigFamily = [...familySmith, 'Lily', ...familyMiller];
+console.log(bigFamily);
+
+const h = document.querySelector('h1');
+const boxes = document.querySelectorAll('.box');
+const all = [h, ...boxes];
+
+Array.from(all).forEach(cur => cur.style.color = 'purple');
